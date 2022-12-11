b72f88b7966ce7a14c9bbc5c956aef1367876b2c
Wanga Kanjala <wangakanjala@gmail.com>
2022-12-11 16:16:21 +0200

    Made few changes
    These have affected a few parts of the app okay maybe the whole thing but it has a significance.

--- script/script.js
+++ script/script.js
@@ -2,8 +2,10 @@ function authenticate(){
     // declare variable that don't change during runtime
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
+    // if credentials are correct
     if(username == "admin" && password == "admin"){
         window.location = "./home.html";
+        document.cookie = username;
     }else{
         const audio = new Audio('./sfx/ping.mp3');
         audio.loop = false;
